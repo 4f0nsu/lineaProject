a27002c729834961c7c53cbcfee92173449767fb
4f0nsu <afonsowvaz@gmail.com>
2025-11-07 14:46:46 +0000

    Messages Test (not going to be aplied)

--- lineaProject/lineaProject/urls.py
+++ lineaProject/lineaProject/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', include('homeApp.urls')),
     path('authenticationApp/', include('authenticationApp.urls')),
     path('postsApp/', include('postsApp.urls')),
+    path('messagesApp/', include('messagesApp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
